40270a3a37891a2f24da35abee93d849aa8507ac
ian fireman <imlbf@cin.ufpe.br>
2017-11-04 21:04:03 -0300

    add python call

--- app.js
+++ app.js
@@ -1,73 +1,8 @@
-var ads1x15 = require('node-ads1x15');
-var chip = 1; //0 for ads1015, 1 for ads1115  
+var spawn = require('child_process').spawn,
+  py = spawn('python', ['readAccelerometer.py']);
 
-//Simple usage (default ADS address on pi 2b or 3):
-var adc = new ads1x15(chip);
-var adc2 = new ads1x15(chip);
-var adc3 = new ads1x15(chip);
+py.stdout.on('data', function (data) {
+  console.log(data);
+});
 
-// Optionally i2c address as (chip, address) or (chip, address, i2c_dev)
-// So to use  /dev/i2c-0 use the line below instead...:
-
-//    var adc = new ads1x15(chip, 0x48, 'dev/i2c-0');
-
-var channel = 1; //channel 0, 1, 2, or 3...  
-var samplesPerSecond = '250'; // see index.js for allowed values for your chip  
-var progGainAmp = '4096'; // see index.js for allowed values for your chip  
-
-//somewhere to store our reading   
-var reading = 0;
-var channel0 = 0;
-var channel1 = 0;
-var channel2 = 0;
-
-if (!adc.busy) {
-  setInterval(function () {
-    getChannel0(function (channel0Data) {
-      channel0 = channel0Data;
-      getChannel1(function (channel1Data) {
-        channel1 = channel1Data;
-        getChannel2(function (channel2Data) {
-          channel2 = channel2Data;
-          console.log(channel0 + " | " + channel1 + " | " + channel2);
-        });
-      });
-    });
-  }, 1000);
-}
-
-function getChannel0(cb) {
-  adc.readADCSingleEnded(0, progGainAmp, samplesPerSecond, function (err, data) {
-    if (err) {
-      //logging / troubleshooting code goes here...  
-      throw err;
-    }
-    // if you made it here, then the data object contains your reading!  
-    cb(data);
-    // any other data processing code goes here...  
-  });
-}
-
-function getChannel1(cb) {
-  adc2.readADCSingleEnded(1, progGainAmp, samplesPerSecond, function (err, data) {
-    if (err) {
-      //logging / troubleshooting code goes here...  
-      throw err;
-    }
-    // if you made it here, then the data object contains your reading!  
-    cb(data);
-    // any other data processing code goes here...  
-  });
-}
-
-function getChannel2(cb) {
-  adc3.readADCSingleEnded(2, progGainAmp, samplesPerSecond, function (err, data) {
-    if (err) {
-      //logging / troubleshooting code goes here...  
-      throw err;
-    }
-    // if you made it here, then the data object contains your reading!  
-    cb(data);
-    // any other data processing code goes here...  
-  });
-}
+py.stdin.end();
