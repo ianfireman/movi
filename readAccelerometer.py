12a4b16c5fce46f624e317be8c678880c1f9478f
ian fireman <imlbf@cin.ufpe.br>
2017-11-04 21:17:54 -0300

    reading python

--- readAccelerometer.py
+++ readAccelerometer.py
@@ -29,6 +29,10 @@ adc = Adafruit_ADS1x15.ADS1115()
 # See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
 GAIN = 1
 
+print('Reading ADS1x15 values, press Ctrl-C to quit...')
+# Print nice channel column headers.
+print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
+print('-' * 37)
 # Main loop.
 while True:
     # Read all the ADC channel values in a list.
@@ -44,6 +48,6 @@ while True:
         # Each value will be a 12 or 16 bit signed integer value depending on the
         # ADC (ADS1015 = 12-bit, ADS1115 = 16-bit).
     # Print the ADC values.
-    print(values[1])
+    print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))
     # Pause for half a second.
     time.sleep(0.5)
